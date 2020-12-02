ecf8ce0e56424936bafc9abbd6356cb1f911b71e
18jwong <18jwong@shcp.edu>
2020-12-02 00:07:00 -0800

    idk what changed, but I'm deleting it from my comp

--- BrackeysJamProject/Assets/Scripts/PlayerController.cs
+++ BrackeysJamProject/Assets/Scripts/PlayerController.cs
@@ -3,18 +3,37 @@
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1f;
+    
+    [Header("Unity Setup Fields")]
+    public Transform counterGroup;
+    public string layerAboveCounter;
+    public string layerBelowCounter;
+
     private static Vector3 target;
+    private SpriteRenderer renderer;
 
     void Start()
     {
         target = transform.position;
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {   
+        // Move
         float step =  speed * Time.deltaTime; // calculate distance to move
         transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+        // Update sorting layer
+        if(transform.position.y > counterGroup.position.y)
+        {
+            renderer.sortingLayerName = layerAboveCounter;
+        }
+        else
+        {
+            renderer.sortingLayerName = layerBelowCounter;
+        }
     }
 
     public static void UpdateTarget(Vector3 t)
